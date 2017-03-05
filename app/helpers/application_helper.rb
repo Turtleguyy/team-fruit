03cc91353e524cf4a37e566dda8348d88424edc3
Zachary Nicoll <github@zdn.me>
2017-03-05 01:13:46 -0500

    Add some date range helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+
+  def last_week
+    start_of_week = Date.today.last_week.beginning_of_day
+    end_of_week   = start_of_week + 1.week
+
+    start_of_week..end_of_week
+  end
+
+  def this_week
+    start_of_week = Date.today.beginning_of_week.beginning_of_day
+    end_of_week   = start_of_week + 1.week
+
+    start_of_week..end_of_week
+  end
+
 end
