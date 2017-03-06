743b169bc66c405af5041b5ffe24536e6acd1c6d
Zachary Nicoll <github@zdn.me>
2017-03-06 01:59:00 -0500

    Add real copy to home page and tweak some styles

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -2,7 +2,7 @@ class VideosController < ApplicationController
 
   def index
     @videos   = Video.where(published_at: helpers.last_week)
-    @featured = @videos.order(:view_count).limit(1).first
+    @featured = @videos.order(view_count: :desc).limit(1).first
 
     if cookies[:apple]
       user = cookies[:apple]
