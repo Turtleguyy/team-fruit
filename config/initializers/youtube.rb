c1f2e48e98194e871a52514399006af4bd699fd2
Zachary Nicoll <github@zdn.me>
2017-03-20 01:13:48 -0400

    Use an environment variable for youtube

--- config/initializers/youtube.rb
+++ config/initializers/youtube.rb
@@ -1,5 +1,4 @@
 Yt.configure do |config|
-  # config.api_key = ENV['YOUTUBE_API_SECRET']
-  config.api_key = "AIzaSyAz_r7_9ZqFo0I3uAMWIql5IUn9NZ0vS-I"
+  config.api_key = ENV['YOUTUBE_API_SECRET']
   config.log_level = :debug
 end
