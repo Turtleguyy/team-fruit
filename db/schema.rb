7a9e2ebfbf6d5cc8bd6414ff38642d4f26fafc73
Zachary Nicoll <github@zdn.me>
2017-03-20 01:39:09 -0400

    Switch to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170307052122) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "videos", force: :cascade do |t|
     t.boolean  "is_featured"
     t.datetime "created_at",    null: false
